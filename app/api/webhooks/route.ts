6664581c5807e23912c7090da4e9c2213c39442c
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2024-09-23 22:13:48 +0300

    add api/webhooks to public routes

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -58,10 +58,18 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
+    // const mongoUser = await createUser({
+    //   clerkId: id,
+    //   name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+    //   username: username!,
+    //   email: email_addresses[0].email_address,
+    //   picture: image_url,
+    // });
+
     const mongoUser = await createUser({
       clerkId: id,
-      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-      username: username!,
+      name: `${first_name}${last_name ? `${last_name}` : ""}`,
+      username: username || "Userx",
       email: email_addresses[0].email_address,
       picture: image_url,
     });
--- middleware.ts
+++ middleware.ts
@@ -16,37 +16,19 @@
 //   ],
 // };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/api/webhook",
-  "/question/:id)",
-  "/tags(.*)",
-  "/tags/:id",
-  "/profile/:id",
-  "/community",
-  "/jobs",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
-
-const isIgnoredRoute = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
-
-export default clerkMiddleware((auth, request) => {
-  const { pathname } = new URL(request.url);
-
-  if (isIgnoredRoute(request)) {
-    return;
-  }
-
-  if (!isPublicRoute(request)) {
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
     auth().protect();
   }
 });
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
