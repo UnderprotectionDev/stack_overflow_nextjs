b5e034661666806eabe4fd58a94ff6783eb887a9
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2024-10-01 04:14:42 +0300

    tag details

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,34 +1,26 @@
 import QuestionCard from "@/components/cards/QuestionCard";
-import { Filter } from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import { LocalSearchbar } from "@/components/shared/search/LocalSearchbar";
-import { QuestionFilters } from "@/constants/filters";
-import { getSavedQuestions } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs/server";
+import { IQuestion } from "@/database/question.model";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import { URLProps } from "@/types";
 
-export default async function Home() {
-  const { userId } = auth();
-  if (!userId) return null;
-
-  const result = await getSavedQuestions({
-    clerkId: userId,
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
   });
-
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
-
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+      <div className="mt-11 w-full">
         <LocalSearchbar
-          route="/"
+          route={`/tags/${params.id}`}
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search for questions"
+          placeholder="Search tag questions"
           otherClasses="flex-1"
-        />
-        <Filter
-          filters={QuestionFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
 
@@ -49,7 +41,7 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There's no question saved to show"
+            title="There's no tag question saved to show"
             description="        Be the first to break the silence! Ask a question and kickstart the
         discussion. Our query could be the next big thing others learn from. Get
         involved!"
@@ -60,4 +52,6 @@ export default async function Home() {
       </div>
     </>
   );
-}
+};
+
+export default Page;
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -152,7 +152,7 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       },
       populate: [
         { path: "tags", model: Tag, select: "_id name" },
-        { path: "author", model: User, select: "_id name" },
+        { path: "author", model: User, select: "_id name picture" },
       ],
     });
 
